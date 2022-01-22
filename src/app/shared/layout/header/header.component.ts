7d1761764295a086f5ba995debabfbe22d74ec45
FuadAsgarov <fuada@crbn.az>
2022-01-22 23:32:18 +0400

    code comment and removed

--- src/app/shared/layout/header/header.component.ts
+++ src/app/shared/layout/header/header.component.ts
@@ -5,41 +5,48 @@ import { map, Observable, startWith } from 'rxjs';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements OnInit {
   displayNavbar!: string;
   control = new FormControl();
-  streets: string[] = ['Champs-Élysées', 'Lombard Street', 'Abbey Road', 'Fifth Avenue'];
+  streets: string[] = [
+    'Champs-Élysées',
+    'Lombard Street',
+    'Abbey Road',
+    'Fifth Avenue',
+  ];
   filteredStreets!: Observable<string[]>;
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     this.displayNavbar = '1';
     this.filteredStreets = this.control.valueChanges.pipe(
       startWith(''),
-      map(value => this._filter(value)),
+      map((value) => this._filter(value))
     );
   }
 
   private _filter(value: string): string[] {
     const filterValue = this._normalizeValue(value);
-    return this.streets.filter(street => this._normalizeValue(street).includes(filterValue));
+    return this.streets.filter((street) =>
+      this._normalizeValue(street).includes(filterValue)
+    );
   }
 
   private _normalizeValue(value: string): string {
     return value.toLowerCase().replace(/\s/g, '');
   }
   toggleNavbar() {
-
-    if(this.displayNavbar == '0') {
-        this.displayNavbar = '1';
+    if (this.displayNavbar == '0') {
+      this.displayNavbar = '1';
       //  alert(this.displayNavbar);
-    } if(this.displayNavbar == '1') {
-    //    alert("1 - Changing to 0");
-        this.displayNavbar = '0';
+    }
+    if (this.displayNavbar == '1') {
+      //    alert("1 - Changing to 0");
+      this.displayNavbar = '0';
     } else {
-        this.displayNavbar = '1';
+      this.displayNavbar = '1';
     }
   }
 }
